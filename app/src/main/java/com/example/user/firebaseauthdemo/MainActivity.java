081d33da1d980e18ae525776d4584643fa26edba
Jeff Han <hanorang2@naver.com>
2017-09-23 12:38:34 +0900

    initial commit

---
+++ app/src/main/java/com/example/user/firebaseauthdemo/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.user.firebaseauthdemo;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignIn;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() != null){
+            //profile activity here
+            finish();
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+        }
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        textViewSignIn = (TextView) findViewById(R.id.textViewSignIn);
+
+        buttonRegister.setOnClickListener(this);
+        textViewSignIn.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //email is empty
+            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+            //stopping the function execution further
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)) {
+            //password is empty
+            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+            //stopping the function execution further
+            return;
+        }
+        //if validation are ok
+        //we will first show a progressbar
+
+        progressDialog.setTitle("Loading...");
+        progressDialog.setMessage("Registering User...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                                finish();
+                                startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                        }else{
+                            Toast.makeText(MainActivity.this, "Could not register.. Try again", Toast.LENGTH_SHORT).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonRegister){
+            registerUser();
+        }
+
+        if(view == textViewSignIn){
+            //will open login activity here
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+}
---
+++ app/src/main/java/com/example/user/firebaseauthdemo/UserInformation.java
@@ -0,0 +1,19 @@
+package com.example.user.firebaseauthdemo;
+
+/**
+ * Created by user on 2017-09-23.
+ */
+
+public class UserInformation {
+
+    public String name;
+    public String address;
+
+    public UserInformation(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public UserInformation(String name) {
+    }
+}
